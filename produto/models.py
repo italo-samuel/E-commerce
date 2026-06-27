8e922d1e3b969e86a6765c6dba8611aa98f0c75d
Protitipo_Hackatom <prototipohackatom@gmail.com>
2026-06-27 13:08:38 -0300

    models produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+'''
+ Produto:
+            nome - Char
+            descricao_curta - Text
+            descricao_longa - Text
+            imagem - Image
+            slug - Slug
+            preco_marketing - Float
+            preco_marketing_promocional - Float
+            tipo - Choices
+                ('V', 'Variável'),
+                ('S', 'Simples'),
+'''
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField(max_length=255)
+    imagem = models.ImageField(upload_to='produto_imagens/%Y/%m', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variável'),
+            ('S', 'Simples')
+        )
+    )
 
-# Create your models here.
+
+'''
+        Variacao:
+            nome - char
+            produto - FK Produto
+            preco - Float
+            preco_promocional - Float
+            estoque - Int
+'''
