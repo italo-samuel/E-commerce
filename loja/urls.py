31d9af3a3c916787050c7fe32bcac616a99e1124
Protitipo_Hackatom <prototipohackatom@gmail.com>
2026-07-05 16:26:45 -0300

    urls de produto

--- loja/urls.py
+++ loja/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('produto.urls')),
+    path('pedido/', include('pedido.urls')),
+    path('perfil/', include('perfil.urls'))
 ]
